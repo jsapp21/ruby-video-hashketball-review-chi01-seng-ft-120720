5a4d8ea14fd2a2b766cbb77518a7de915a4c1dea
James Sapp <jameseisapp@gmail.com>
2020-12-03 18:23:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,7 +129,7 @@ end
 
 # returns the number of points scored for that player
 def num_points_scored(player_name)
-  game_hash.each do |item, index|
+  game_hash.each do |team_side, index|
     binding.pry 
   end 
 end 
